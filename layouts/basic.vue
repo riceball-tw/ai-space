bac487c637a48a12d3859c3ceb9babe10284d72b
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-02-03 11:45:20 +0800

    feat: Add navbar and related layout

---
+++ layouts/basic.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <AppNavbar class="h-20" />
+    <div class="mt-20">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <p>Some default layout content shared across all pages</p>
-    <slot />
-  </div>
+  <slot />
 </template>
+
+<script setup lang="ts">
+</script>
