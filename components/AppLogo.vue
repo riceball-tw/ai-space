e02a41d1c481f627cefc22d4cc567d2374aa2250
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-02-08 17:00:40 +0800

    feat: Add better logo and cleanup nav item

---
+++ components/AppLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLink to="/" class="inline-flex gap-2 items-center">
+    <Sparkles class="-mb-0.5" />
+    <span class="font-bold text-lg">AiSpace</span>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import { Sparkles } from 'lucide-vue-next';
+</script>
--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,14 +1,9 @@
 <template>
   <nav class="fixed top-0 left-0 right-0 z-10 flex justify-between items-center mx-auto container">
-    <NuxtLink class="p-4 inline-block text-xl font-medium" to="/">iSpace</NuxtLink>
+    <AppLogo />
     <ul class="flex gap-4 text-lg">
-      <!-- <li><NuxtLink to="/">iSpace</NuxtLink></li> -->
-      <li><NuxtLink class="p-4 inline-block" to="/">Home</NuxtLink></li>
       <li><NuxtLink class="p-4 inline-block" to="/profile">Profile</NuxtLink></li>
       <li><NuxtLink class="p-4 inline-block" to="/chat">Chat</NuxtLink></li>
     </ul>
   </nav>
 </template>
-
-<script setup lang="ts">
-</script>
