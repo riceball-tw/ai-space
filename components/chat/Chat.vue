1ea5acc9a882bc219c2d9eb6797d38f642a25c20
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-02-08 17:01:35 +0800

    feat: Add chat component

---
+++ components/chat/Chat.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :class="`flex gap-2 ${direction === 'right' ? 'flex-row-reverse' : ''}`">
+    <Avatar>
+      <AvatarImage :src="avatar.src" :alt="avatar.alt" />
+      <AvatarFallback>{{ avatar.alt }}</AvatarFallback>
+    </Avatar>
+
+    <ChatBubble time="12:00">
+      <slot />
+    </ChatBubble>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import ChatBubble from '@/components/chat/ChatBubble.vue';
+
+  const { avatar, direction = 'left' } = defineProps<{
+    avatar: {
+      src: string;
+      alt: string;
+    };
+    direction?: 'left' | 'right';
+  }>()
+
+</script>
---
+++ components/chat/ChatBubble.vue
@@ -0,0 +1,14 @@
+<template>
+  <div style="max-width: 40ch" class="flex flex-col bg-primary text-primary-foreground px-4 py-2 rounded">
+    <p>
+      <slot />
+    </p>
+    <time class="self-end text-sm" :datetime="time">{{ time }}</time>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const { time } = defineProps<{
+    time: string;
+  }>();
+</script>
