e9dae5a518fc0c4bbb8fd0e23edaf06143f7124a
Riceball <42161586+riceball-tw@users.noreply.github.com>
2025-02-09 20:10:26 +0800

    fix: Supabase auth session not update in time

--- server/api/auth/sign-in.post.ts
+++ server/api/auth/sign-in.post.ts
@@ -11,9 +11,16 @@ export default defineEventHandler(async (event) => {
   }
 
   const { email, password } = validatedBody.data;
-  const { error } = await supabase.auth.signInWithPassword({ email, password });
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
   if (error) {
     return createError({ statusCode: 401, statusMessage: error.message });
   }
+  // 1. For unknown reason, build-in redirect will not work when first time sign in...
+  // 2. useSupabaseUser() is not updated until I leave the browser window or switch browser tabs
+  // Related issues:
+  // - useSupabaseUser() is not updated until I leave the browser window or switch browser tabs: https://github.com/nuxt-modules/supabase/issues/144
+  // Solution: 
+  // - Necessary to use client.auth.setSession() on login: https://github.com/nuxt-modules/supabase/issues/143
+  supabase.auth.setSession(data.session)
   return { message: 'Sign In successful' };
 });
